ec177d98ca3e3bd3d03ea24f61d34bad5d3b28eb
DianaSMer <anaidmerloi@gmail.com>
2024-01-12 17:02:54 -0600

    Ejerciciologicajs05

---
+++ js05practicalogica.js
@@ -0,0 +1,22 @@
+//Ejercicio 1
+
+let userName = prompt("Aqui va el nombre");
+let age = prompt ("Aqui va la edad");
+let favMovies = prompt ("Aqui va las pelis");
+
+//se supone que así guarda todo
+const datos = {
+    userName: userName,
+    age: age,
+    favMovies: favMovies
+
+};
+
+console.log("Datos" + datos.userName)
+console.log("Age" + datos.age);
+console.log ("Movies" + datos.favMovies);
+
+
+
+
+
